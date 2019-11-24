25890dc2122db62759974ea1efdf2e672b963428
Brad Dunn <Brad@autobooks.co>
2019-11-24 13:46:49 -0500

    Some config stuff and typings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `BradDunn.com`,
+    description: `The personal website of Brad Dunn, a software developer`,
+    author: `@bdunn313`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
